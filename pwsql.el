42646c4f6871845122ced828cfa2f6bbcc3d411e
gicrisf <giovanni.crisalfi@protonmail.com>
2024-03-28 16:28:25 +0100

    tree expansion and async support

--- pwsql.el
+++ pwsql.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.2
 ;; Keywords: data tools sql
 ;; Homepage: https://github.com/gicrisf/pwsql
-;; Package-Requires: ((emacs "24.4"))
+;; Package-Requires: ((emacs "25.1"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -19,6 +19,7 @@
 ;;
 ;;; Code:
 (require 'dash)
+(require 'deferred)
 
 (defvar pwsql-server-instance "PWSQL-INSTANCE")
 (defvar pwsql-username "pwsql-usr")
@@ -56,39 +57,128 @@ Return a json string. You can explicit it in ARGS or you can ask for a csv strin
 
 (defun pwsql--display-as-org-table (data-tree)
   "Display a pwsql DATA-TREE as a table in an 'org-mode' buffer."
-  (let* ((syms (mapcar (lambda (x) (car x)) data-tree))
-         ; You just got titles as symbols from the results' tree
-         ; Now, you collect the respective strings.
-         (titles (mapcar (lambda (x) (string-trim (symbol-name x))) syms))
-         ; Values are given as vectors
-         (vecs (mapcar (lambda (x) (cdr x)) data-tree))
-         ; Convert to sequences
-         (cols (mapcar (lambda (x) (append x nil)) vecs))
-         ; Matrix rotation
-         (vals (apply #'-zip-lists cols)))
-
-    ; Properly nested sequences
-    `(,titles
-      ,@vals)))
+  (if (vectorp (cdr (car data-tree)))
+        ; Get titles as symbols from the results' tree
+      (let* ((syms (mapcar (lambda (x) (car x)) data-tree))
+                ; Collect the respective strings.
+             (titles (mapcar (lambda (x) (string-trim (symbol-name x))) syms))
+                ; Values are given as vectors
+             (vecs (mapcar (lambda (x) (cdr x)) data-tree))
+                ; Convert to sequences
+             (cols (mapcar (lambda (x) (append x nil)) vecs))
+                ; Matrix rotation
+             (vals (apply #'-zip-lists cols)))
+        ; Properly nested sequences
+        `(,titles
+          ,@vals))
+    (message "Cannot format as table:\n%s" (pp data-tree))))
 
 (defun pwsql--table-as-plist (table)
-  "Convert an org TABLE in a comfortable plist."
-  (mapcar (lambda (value)
-            (cl-labels ((recursive-zipper
-                         (symnames resvalues acc)
-                         ; table titles, single result value, accumulator
-                         (if (null resvalues)
-                             acc
-                           (let ((plist-el (list (intern (concat ":" (car symnames)))
-                                                 (car resvalues))))
-                             (recursive-zipper (cdr symnames)
-                                               (cdr resvalues)
-                                               (append plist-el acc))))))
+  "Convert an org TABLE in a comfortable plist.
+e.g. goes from =((1 2 3) (4 5 6))=
+to =((:3 6 :2 5 :1 4))="
+  (cl-labels
+      ((recursive-zipper
+        (symnames resvalues acc)
+        ; "table titles, single result value, accumulator"
+        (if (null resvalues)
+            acc
+          (let ((plist-el (list
+                           (intern (concat ":" (format "%s" (car symnames))))
+                                (car resvalues))))
+            (recursive-zipper (cdr symnames)
+                              (cdr resvalues)
+                              (append plist-el acc))))))
+    (mapcar (lambda (value)
               (recursive-zipper
                (car table)
-               (cdr value)
-               nil)))
-          (cdr table)))
+               value
+               nil))
+            (cdr table))))
+
+(defun pwsql--expand-tree (res-tree)
+  (cl-labels
+      ((recexpand
+        (res-tree acc)
+        (if (or (null (cdr (car res-tree)))
+                (< (seq-length (cdr (car res-tree))) 1))
+            acc
+          (let ((new-acc (cons
+                          (mapcar
+                           (lambda (el)
+                             (cons (car el) (aref (cdr el) 0)))
+                           res-tree)
+                          acc))
+                (new-res-tree
+                 (mapcar
+                  (lambda (el)
+                    (cons (car el) (seq-drop (cdr el) 1)))
+                  res-tree)))
+            (recexpand new-res-tree new-acc)))))
+  (if (not (vectorp (cdr (car res-tree))))
+      (list res-tree)
+    (recexpand res-tree nil))))
+
+(defun pwsql-get (query &rest args)
+  (let* ((as (plist-get args :as))
+         (as (if as as 'tree))
+         (res (if (or (equal as 'raw)
+                      (equal as 'tree)
+                      (equal as 'table)
+                      (equal as 'expanded-tree))
+                  (pwsql--query query)
+                (error "The provided :as value is not a viable option")))
+         (res (if (or (equal as 'tree)
+                      (equal as 'table)
+                      (equal as 'expanded-tree))
+                  (json-read-from-string res)
+                res))
+         (res (if (equal as 'table)
+                  (pwsql--display-as-org-table res)
+                res))
+         (res (if (equal as 'expanded-tree)
+                  (pwsql--expand-tree res)
+                res)))
+    res))
+
+(defun pwsql-async-get (query &rest args)
+  (lexical-let* ((as (plist-get args :as))
+                 ;; defaults on tree
+                 (as (if as as 'tree))
+                 ;; check :as and pass query if it's fine
+                 (query (if (or (equal as 'raw)
+                                (equal as 'tree)
+                                (equal as 'table)
+                                (equal as 'expanded-tree))
+                            query
+                          (error "The provided :as value is not a viable option"))))
+    (deferred:$
+      (deferred:next
+        (lambda ()
+          (pwsql--query query)))
+      (deferred:error it
+        (lambda (e)
+          (message "PWSQL query error: %s" e)))
+      (deferred:nextc it
+        (lambda (res)
+          (if (or (equal as 'tree)
+                  (equal as 'table)
+                  (equal as 'expanded-tree))
+              (json-read-from-string res)
+            res)))
+      (deferred:nextc it
+        (lambda (res)
+          (if (equal as 'table)
+              (pwsql--display-as-org-table res)
+            res)))
+      (deferred:nextc it
+        (lambda (res)
+          (if (equal as 'expanded-tree)
+              (pwsql--expand-tree res)
+            res)))
+      (deferred:error it
+        (lambda (e)
+          (message "PWSQL conversion error: %s" e))))))
 
 (provide 'pwsql)
 ;;; pwsql.el ends here
